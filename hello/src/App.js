605bf23f9ac89b49e35a190be31e2282587ef314
mehedihassan777 <mehedi15-5815@diu.edu.bd>
2022-07-05 19:29:59 +0600

    commit 10

--- hello/src/App.js
+++ hello/src/App.js
@@ -2,9 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet'
 import Welcome from './components/Welcome';
+import Message from './components/Message';
 function App() {
   return (
     <div className="App">
+      <Message />
       <Greet name="Lemon" heroname="Spiderman"><p>This is children</p></Greet>
       <Greet name="Mim" heroname="Superman"><button>Child Button</button></Greet>
       <Greet name="Jim" heroname="Batman"></Greet>
